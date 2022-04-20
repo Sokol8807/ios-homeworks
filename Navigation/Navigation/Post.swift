9323c3b19edd5be6a3216f15c4cc654e49be663d
Sokol8807 <sokol8807@gmail.com>
2022-04-20 22:16:30 +0300

    Добавил в общий репозиторий проект Navigation

---
+++ Navigation/Navigation/Post.swift
@@ -0,0 +1,14 @@
+//
+//  PostStruct.swift
+//  Navigation
+//
+//  Created by Ilya Sokolov on 18.04.2022.
+//
+
+import Foundation
+
+// создаю структуру Пост
+
+struct Post {
+    var title: String
+}
