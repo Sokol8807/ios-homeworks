3860c467cb7a48346c36403a0e288fd09d1bfdd2
Sokol8807 <sokol8807@gmail.com>
2022-04-27 16:08:05 +0300

    Выполнил все пункты ДЗ 2/2 из обязательной части

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -4,24 +4,57 @@ import UIKit
 class ProfileViewController: UIViewController {
     
     // создаю экземпляр класса ProfileHeaderView
-    var profileHeaderView = ProfileHeaderView()
+    let profileHeaderView = ProfileHeaderView()
+    
+    let newButton: UIButton = {
+        
+        let buttonNew = UIButton()
+        buttonNew.translatesAutoresizingMaskIntoConstraints = false
+        buttonNew.setTitle("NewPost", for: .normal)
+        buttonNew.backgroundColor = .blue
+        
+        return buttonNew
+    } ()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .lightGray
+        setupAutoLayuot()
+        
+       
+    }
+    
+    private func setupAutoLayuot (){
+        
         view.addSubview(profileHeaderView)   //Добавляю profileHV в качестве subview
-//        profileHeaderView.translatesAutoresizingMaskIntoConstraints.self = false
-//        profileHeaderView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-//        profileHeaderView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-//        profileHeaderView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
-//        profileHeaderView.widthAnchor.constraint(equalToConstant: 220).isActive = true
+        view.addSubview(newButton)
+        
+        profileHeaderView.translatesAutoresizingMaskIntoConstraints.self = false
+        
+        
+        NSLayoutConstraint.activate([
+            
+            //автолайот для profileHeaderView
+            
+            profileHeaderView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            profileHeaderView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            profileHeaderView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            profileHeaderView.heightAnchor.constraint(equalToConstant: 220),
+            
+            
+            //автолайот для newButton
+            
+            newButton.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            newButton.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            newButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            newButton.heightAnchor.constraint(equalToConstant: 40)
+            
+        
+        ])
+        
         
     }
+   
     
-    override func viewWillLayoutSubviews() {
-        
-        super.viewWillLayoutSubviews()
-        profileHeaderView.frame = view.bounds
-    
-    }
 }
