f7f7d1e4afc91159e9c4a09191f4dd9807ae5d6f
Sokol8807 <sokol8807@gmail.com>
2022-04-28 14:31:53 +0300

    Добавил дополнительное задание 2.2 UIStackView в FeedViewControler

--- Navigation/Navigation/FeedViewController.swift
+++ Navigation/Navigation/FeedViewController.swift
@@ -18,24 +18,63 @@ class FeedViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemCyan
-        makeButton()  // вызываем кнопку в viewDidLoad
-        
-    }
-    private func makeButton() {
-        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 220, height: 50))
-        button.center = view.center
-        button.setTitle("Пост", for: .normal)
-        button.backgroundColor = .black
-        button.addTarget(self, action: #selector(tapAction), for: .touchUpInside)
-        view.addSubview(button)  // добавляет кнопку на экран
-    }
-
-    @objc private func tapAction(){
-       let postViewContoller = PostViewController()// Добавляем действия со ссылкой на третий экран
-        postViewContoller.titlePost = post.title
-        navigationController?.pushViewController(postViewContoller, animated: true)
-    }
-
-    
-    }
-    
+                stackVc()
+                
+            }
+            
+            private let stackView: UIStackView = {
+                let viewStack = UIStackView()
+                viewStack.translatesAutoresizingMaskIntoConstraints = false
+                viewStack.axis = .vertical
+                viewStack.distribution = .fillEqually
+                viewStack.spacing = 10
+                return viewStack
+            }()
+            
+            private lazy var setButton: UIButton = {
+                let button = UIButton()
+                button.translatesAutoresizingMaskIntoConstraints = false
+                button.backgroundColor = .systemMint
+                button.tintColor = .white
+                button.setTitle("Go to Post", for: .normal)
+                button.layer.cornerRadius = 5
+                button.addTarget(self, action: #selector(tapAction), for: .touchUpInside)
+                return button
+            }()
+            
+            @objc private func tapAction() {
+                let postViewController = PostViewController()
+               // self.post = Post(title: "Post")
+                navigationController?.pushViewController(postViewController, animated: true)
+            }
+            
+            private lazy var viewButton: UIButton = {
+                let button = UIButton()
+                button.translatesAutoresizingMaskIntoConstraints = false
+                button.backgroundColor = .label
+                button.tintColor = .white
+                button.setTitle("Go to Post", for: .normal)
+                button.layer.cornerRadius = 5
+                button.addTarget(self, action: #selector(didAction), for: .touchUpInside)
+                return button
+            }()
+            
+            @objc private func didAction() {
+                let postViewController = PostViewController()
+                //self.post = Post(title: "Post")
+                navigationController?.pushViewController(postViewController, animated: true)
+            }
+            
+            private func stackVc() {
+                view.addSubview(stackView)
+                [setButton, viewButton] .forEach { stackView.addArrangedSubview($0) }
+                
+                NSLayoutConstraint.activate([
+                    stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                    stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+                    stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+                    stackView.heightAnchor.constraint(equalToConstant: 100),
+                ])
+            }
+            
+        }
